6c44b1530c8177e32df8d6c27cc932742139a71d
van53 <53ivanivanov28@gmail.com>
2026-06-13 15:55:13 +0300

    fix: adjust FileTypeValidator to properly validate image mime types

--- lab11/backend/src/files/files.controller.ts
+++ lab11/backend/src/files/files.controller.ts
@@ -28,7 +28,7 @@ export class FilesController {
       new ParseFilePipe({
         validators: [
           new MaxFileSizeValidator({ maxSize: 5 * 1024 * 1024 }),
-          new FileTypeValidator({ fileType: '.(png|jpeg|jpg|webp)' }),
+          new FileTypeValidator({ fileType: /image\/(jpeg|png|webp|jpg)/ }),
         ],
       }),
     )
